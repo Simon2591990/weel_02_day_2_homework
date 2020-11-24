1401f95a6d1701dd6ee7957d55528ec913c7754e
Simon Charters <simon259@hotmail.co.uk>
2020-11-24 16:44:00 +0000

    initial commit

---
+++ testing_start_code/tests/calculator_test.py
@@ -0,0 +1,21 @@
+import unittest
+from src.calculator import add, divide, multiply, subtract
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(6, add(2,4))
+
+    def test_subtract(self):
+        self.assertEqual(6, subtract(12, 6))
+
+    def test_multiply(self):
+        self.assertEqual(10, multiply(2,5))
+
+    def test_divide(self):
+        self.assertEqual(5, divide(25,5))
+
+
+    
+
+
+
